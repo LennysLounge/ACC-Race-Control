63d725f8dd28e966a3edbb993cd69212d7062c2b
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-09-04 01:02:13 +0200

    Add more data to the list entry and the example data.

--- src/main/java/ACCLiveTiming/extensions/livetiming/ListEntry.java
+++ src/main/java/ACCLiveTiming/extensions/livetiming/ListEntry.java
@@ -18,16 +18,34 @@ public class ListEntry {
     private final String position;
     private final String name;
     private final String carNumber;
-    private final String time;
+    private final String lapCount;
+    private final String gap;
+    private final String toLeader;
+    private final String delta;
+    private final String currentLap;
+    private final String sectorOne;
+    private final String sectorTwo;
+    private final String sectorThree;
+    private final String bestLap;
+    private final String lastLap;
+
     private final boolean inPits;
     private final DriverCategory category;
 
-    public ListEntry(String position, String name, String carNumber, String time,
-            boolean inPits, DriverCategory category) {
+    public ListEntry(String position, String name, String carNumber, String lapCount, String gap, String toLeader, String delta, String currentLap, String sectorOne, String sectorTwo, String sectorThree, String bestLap, String lastLap, boolean inPits, DriverCategory category) {
         this.position = position;
         this.name = name;
         this.carNumber = carNumber;
-        this.time = time;
+        this.lapCount = lapCount;
+        this.gap = gap;
+        this.toLeader = toLeader;
+        this.delta = delta;
+        this.currentLap = currentLap;
+        this.sectorOne = sectorOne;
+        this.sectorTwo = sectorTwo;
+        this.sectorThree = sectorThree;
+        this.bestLap = bestLap;
+        this.lastLap = lastLap;
         this.inPits = inPits;
         this.category = category;
     }
@@ -44,8 +62,44 @@ public class ListEntry {
         return carNumber;
     }
 
-    public String getTime() {
-        return time;
+    public String getLapCount() {
+        return lapCount;
+    }
+
+    public String getGap() {
+        return gap;
+    }
+
+    public String getToLeader() {
+        return toLeader;
+    }
+
+    public String getDelta() {
+        return delta;
+    }
+
+    public String getCurrentLap() {
+        return currentLap;
+    }
+
+    public String getSectorOne() {
+        return sectorOne;
+    }
+
+    public String getSectorTwo() {
+        return sectorTwo;
+    }
+
+    public String getSectorThree() {
+        return sectorThree;
+    }
+
+    public String getBestLap() {
+        return bestLap;
+    }
+
+    public String getLastLap() {
+        return lastLap;
     }
 
     public boolean isInPits() {
@@ -55,7 +109,5 @@ public class ListEntry {
     public DriverCategory getCategory() {
         return category;
     }
-    
 
-    
 }
--- src/main/java/ACCLiveTiming/extensions/livetiming/LiveTimingExtension.java
+++ src/main/java/ACCLiveTiming/extensions/livetiming/LiveTimingExtension.java
@@ -32,10 +32,14 @@ public class LiveTimingExtension extends AccClientExtension {
 
     public LiveTimingExtension() {
         this.panel = new LiveTimingPanel(this);
-        entries.add(new ListEntry("1", "K. Bond", "80", "01:49.353", false, DriverCategory.SILVER));
-        entries.add(new ListEntry("2", "P. Hold [FRT]", "81", "+2.427", false, DriverCategory.SILVER));
-        entries.add(new ListEntry("3", "D. Almeida", "18", "+3.306", false, DriverCategory.BRONZE));
-        entries.add(new ListEntry("4", "E. Rincon", "10", "--", true, DriverCategory.BRONZE));
+        entries.add(new ListEntry("1", "K. Bond", "80", "12", "--.--", "--.--", "-0.52", "1:00.12", "12.02", "16.45", "18.64", "1:37.59", "1:38.21",
+                false, DriverCategory.SILVER));
+        entries.add(new ListEntry("2", "P. Hold [FRT]", "81", "12", "+2.52", "+2.52", "+2.34", "55.12", "12.08", "16.56", "18.69", "1.38.59", "1:38.59",
+                true, DriverCategory.SILVER));
+        entries.add(new ListEntry("3", "D. Almeida", "18", "12", "+4.82", "+7.34", "-0.02", "0:40.89", "12.20", "16.57", "18.81", "1:38.13", "1:38.59",
+                false, DriverCategory.BRONZE));
+        entries.add(new ListEntry("4", "E. Rincon", "10", "11", "+6.23", "+13.56", "+0.59", "0:20.55", "12.17", "16.64", "18.71", "1:38.37", "1:38.65",
+                true, DriverCategory.GOLD));
     }
 
     public List<ListEntry> getEntries() {
