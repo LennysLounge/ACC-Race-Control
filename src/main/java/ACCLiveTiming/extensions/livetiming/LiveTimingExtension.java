891b8f88312a4169cb3c4ee6f97621f8ce2a0e68
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-09-03 16:35:22 +0200

    Add example data for testing.

--- src/main/java/ACCLiveTiming/extensions/livetiming/LiveTimingExtension.java
+++ src/main/java/ACCLiveTiming/extensions/livetiming/LiveTimingExtension.java
@@ -7,6 +7,12 @@ package ACCLiveTiming.extensions.livetiming;
 
 import ACCLiveTiming.client.AccClientExtension;
 import ACCLiveTiming.networking.data.AccBroadcastingData;
+import ACCLiveTiming.networking.data.RealtimeInfo;
+import ACCLiveTiming.networking.enums.DriverCategory;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -15,13 +21,33 @@ import java.util.logging.Logger;
  */
 public class LiveTimingExtension extends AccClientExtension {
 
+    /**
+     * This classes logger.
+     */
     private static Logger LOG = Logger.getLogger(LiveTimingExtension.class.getName());
-    
+    /**
+     * Map of entries to their carId.
+     */
+    private List<ListEntry> entries = new LinkedList<>();
+
     public LiveTimingExtension() {
         this.panel = new LiveTimingPanel(this);
+        entries.add(new ListEntry(Arrays.asList("1", "K. Bond", "80", "01:49.353"), false, DriverCategory.SILVER));
+        entries.add(new ListEntry(Arrays.asList("2", "P. Hold [FRT]", "81", "+2.427"), false, DriverCategory.SILVER));
+        entries.add(new ListEntry(Arrays.asList("3", "D. Almeida", "18", "+3.306"), false, DriverCategory.BRONZE));
+        entries.add(new ListEntry(Arrays.asList("4", "E. Rincon", "10", "--"), true, DriverCategory.BRONZE));
     }
-    
-    public AccBroadcastingData getModel(){
+
+    public List<ListEntry> getEntries() {
+        return Collections.unmodifiableList(entries);
+    }
+
+    public AccBroadcastingData getModel() {
         return client.getModel();
     }
+
+    @Override
+    public void onRealtimeCarUpdate(RealtimeInfo info) {
+    }
+
 }
