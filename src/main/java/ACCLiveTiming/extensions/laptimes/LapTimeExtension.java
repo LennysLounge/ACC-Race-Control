2fd4f3763831ddb0b5ec0eb0b4d69125d3714ea0
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-09-08 21:02:33 +0200

    Find when a lap is completed manually.

--- src/main/java/ACCLiveTiming/extensions/laptimes/LapTimeExtension.java
+++ src/main/java/ACCLiveTiming/extensions/laptimes/LapTimeExtension.java
@@ -7,8 +7,14 @@ package ACCLiveTiming.extensions.laptimes;
 
 import ACCLiveTiming.client.AccClientExtension;
 import ACCLiveTiming.extensions.incidents.IncidentExtension;
-import ACCLiveTiming.extensions.logging.LoggingExtension;
 import ACCLiveTiming.networking.data.BroadcastingEvent;
+import ACCLiveTiming.networking.data.CarInfo;
+import ACCLiveTiming.networking.data.LapInfo;
+import ACCLiveTiming.networking.data.RealtimeInfo;
+import ACCLiveTiming.networking.enums.LapType;
+import ACCLiveTiming.utility.TimeUtils;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -19,6 +25,8 @@ public class LapTimeExtension extends AccClientExtension {
 
     private static Logger LOG = Logger.getLogger(IncidentExtension.class.getName());
 
+    private final Map<Integer, Integer> lapCount = new HashMap<>();
+
     public LapTimeExtension() {
     }
 
@@ -28,29 +36,52 @@ public class LapTimeExtension extends AccClientExtension {
     }
 
     @Override
+    public void onRealtimeCarUpdate(RealtimeInfo info) {
+        if (lapCount.containsKey(info.getCarId())) {
+            if (lapCount.get(info.getCarId()) != info.getLaps()) {
+                LapInfo lap = info.getLastLap();
+                boolean isPersonalBest = lap.getLapTimeMS() == info.getBestSessionLap().getLapTimeMS();
+                boolean isSessionBest = lap.getLapTimeMS() == client.getModel().getSessionInfo().getBestSessionLap().getLapTimeMS();
+                onLapComplete(lap, isPersonalBest, isSessionBest);
+            }
+        }
+        lapCount.put(info.getCarId(), info.getLaps());
+    }
+
+    private void onLapComplete(LapInfo lap, boolean isPB, boolean isSB) {
+        CarInfo car = client.getModel().getCar(lap.getCarId());
+
+        String message = "Lap completed: #" + car.getCarNumber()
+                + "\t" + TimeUtils.asLapTime(lap.getLapTimeMS()) + "\t";
+        if (isPB) {
+            message += "[Personal Best]";
+        }
+        if (isSB) {
+            message += "[Session Best]";
+        }
+        if (lap.getType() == LapType.INLAP) {
+            message += "[Inlap]";
+        }
+        if (lap.getType() == LapType.OUTLAP) {
+            message += "[Outlap]";
+        }
+        client.log(message);
+        LOG.info(message);
+    }
+
+    @Override
     public void onLapComplete(BroadcastingEvent event) {
-        int carNumber = client.getModel().getCar(event.getCarId()).getCarNumber();
-        String msg = "Lap completed: #" + carNumber + "\t" + event.getMessage();
-        LOG.info(msg);
-        client.log(msg);
+
     }
 
     @Override
     public void onBestSessionLap(BroadcastingEvent event) {
-        int carNumber = client.getModel().getCar(event.getCarId()).getCarNumber();
-        String msg = "Lap completed: #" + carNumber + "\t" + event.getMessage() 
-                + "\t[Session best]";
-        LOG.info(msg);
-        client.log(msg);
+
     }
 
     @Override
     public void onBestPersonalLap(BroadcastingEvent event) {
-        int carNumber = client.getModel().getCar(event.getCarId()).getCarNumber();
-        String msg = "Lap completed: #" + carNumber + "\t" + event.getMessage() 
-                + "\t[Personal best]";
-        LOG.info(msg);
-        client.log(msg);
+
     }
 
 }
