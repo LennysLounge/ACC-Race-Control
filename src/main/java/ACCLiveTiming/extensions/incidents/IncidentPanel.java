bca74d85d37a4864c3dbc1dc7e30f3de22c63d2e
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-09-12 20:11:35 +0200

    Redo the incident panel.

--- src/main/java/ACCLiveTiming/extensions/incidents/IncidentPanel.java
+++ src/main/java/ACCLiveTiming/extensions/incidents/IncidentPanel.java
@@ -6,14 +6,15 @@
 package ACCLiveTiming.extensions.incidents;
 
 import ACCLiveTiming.extensions.ExtensionPanel;
+import ACCLiveTiming.networking.data.CarInfo;
 import ACCLiveTiming.networking.enums.SessionType;
 import ACCLiveTiming.utility.TimeUtils;
 import ACCLiveTiming.visualisation.LookAndFeel;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import static processing.core.PConstants.CENTER;
 import static processing.core.PConstants.LEFT;
+import static processing.core.PConstants.RIGHT;
 
 /**
  *
@@ -21,9 +22,20 @@ import static processing.core.PConstants.LEFT;
  */
 public class IncidentPanel extends ExtensionPanel {
 
-    private IncidentExtension extension;
+    private final IncidentExtension extension;
 
-    private int scroll;
+    private int scroll = 0;
+
+    private int visibleRows = 0;
+
+    private List<Accident> incidents = new LinkedList<>();
+
+    private final Column[] columns = {
+        new Column("", 0.4f, false, LEFT),
+        new Column("Nr.", 1, false, CENTER),
+        new Column("Session Time", 6.4f, false, LEFT),
+        new Column("#", 1.25f, true, LEFT)
+    };
 
     public IncidentPanel(IncidentExtension extension) {
         this.extension = extension;
@@ -32,67 +44,183 @@ public class IncidentPanel extends ExtensionPanel {
 
     @Override
     public void drawPanel() {
-        int lineHeight = LookAndFeel.get().LINE_HEIGHT;
+        LookAndFeel laf = LookAndFeel.get();
+        incidents = extension.getAccidents();
 
-        List<Accident> accidents = new LinkedList<>();
-        accidents.addAll(extension.getAccidents());
-        Collections.reverse(accidents);
-
-        if (scroll < 0) {
-            scroll = 0;
-        }
-        int visibleLines = applet.height / lineHeight;
-        if (scroll > accidents.size() - visibleLines) {
-            scroll = accidents.size() - visibleLines;
+        //Draw Header
+        applet.fill(laf.COLOR_DARK_DARK_GRAY);
+        applet.rect(0, 0, width, laf.LINE_HEIGHT);
+        applet.fill(laf.COLOR_WHITE);
+        for (Column column : columns) {
+            drawCellText(column.head, column, 0, laf.COLOR_WHITE);
         }
 
-        int n = 0;
-        int scrollCount = scroll;
-        for (Accident accident : accidents) {
-            if (scrollCount-- > 0) {
+        //Draw background
+        int rows = Math.min(visibleRows, incidents.size() + 1);
+        int yOffset = laf.LINE_HEIGHT;
+        for (int i = 1; i < rows; i++) {
+            applet.fill((i + scroll) % 2 == 0 ? laf.COLOR_DARK_DARK_GRAY : laf.COLOR_DARK_GRAY);
+            applet.rect(0, yOffset, width, laf.LINE_HEIGHT);
+            yOffset += laf.LINE_HEIGHT;
+        }
+        applet.fill(laf.COLOR_DARK_GRAY);
+        applet.rect(0, yOffset, width, height - yOffset);
+
+        int n = 1;
+        int scrollSkip = scroll;
+        for (Accident accident : incidents) {
+            if (scrollSkip-- > 0) {
                 continue;
             }
 
-            int y = lineHeight * n++;
-            int x = 20;
-
-            applet.fill((n % 2 == 0) ? 50 : 40);
-            applet.stroke((n % 2 == 0) ? 50 : 40);
-
-            applet.rect(0, y, applet.width, lineHeight);
-
-            if (accident.getSessionID().getType() == SessionType.PRACTICE) {
-                applet.fill(LookAndFeel.get().COLOR_PRACTICE);
-            } else if (accident.getSessionID().getType() == SessionType.QUALIFYING) {
-                applet.fill(LookAndFeel.get().COLOR_QUALIFYING);
-            } else if (accident.getSessionID().getType() == SessionType.RACE) {
-                applet.fill(LookAndFeel.get().COLOR_RACE);
+            switch (accident.getSessionID().getType()) {
+                case PRACTICE:
+                    drawCellBackground(columns[1], n, laf.COLOR_PRACTICE);
+                    break;
+                case QUALIFYING:
+                    drawCellBackground(columns[1], n, laf.COLOR_QUALIFYING);
+                    break;
+                case RACE:
+                    drawCellBackground(columns[1], n, laf.COLOR_RACE);
+                    break;
+                default:
+                    break;
             }
+            drawCellText(String.valueOf(accident.getIncidentNumber()), columns[1], n, laf.COLOR_WHITE);
+            drawCellText(TimeUtils.asDuration(accident.getEarliestTime()), columns[2], n, laf.COLOR_WHITE);
 
-            applet.rect(x, y, lineHeight, lineHeight);
-            applet.fill(255);
-            applet.textAlign(CENTER, CENTER);
-            applet.text("" + accident.getIncidentNumber(), x + lineHeight / 2, y + lineHeight / 2);
-            applet.textAlign(LEFT, CENTER);
-            applet.text(TimeUtils.asDuration(accident.getEarliestTime()),
-                    x + lineHeight + 20, y + lineHeight / 2);
-
-            int m = 0;
+            float x = columns[3].xOffset * laf.LINE_HEIGHT;
             for (int carId : accident.getCars()) {
-                int xx = (int) (lineHeight * 1.5f * m++);
-                applet.fill(LookAndFeel.get().COLOR_RED);
-                applet.rect(300 + xx, y, lineHeight * 1.5f, lineHeight);
-                applet.fill(255);
-                applet.textAlign(CENTER, CENTER);
-                applet.text("" + extension.getModel().getCar(carId).getCarNumber(),
-                        300 + xx + lineHeight * 1.5f / 2, y + lineHeight / 2);
+                CarInfo car = extension.getModel().getCar(carId);
+                String carNumber = String.valueOf(extension.getModel().getCar(carId).getCarNumber());
+
+                int background_color = 0;
+                int text_color = 0;
+
+                switch (car.getDriver().getCategory()) {
+                    case BRONZE:
+                        background_color = laf.COLOR_RED;
+                        text_color = laf.COLOR_BLACK;
+                        break;
+                    case SILVER:
+                        background_color = laf.COLOR_GRAY;
+                        text_color = laf.COLOR_WHITE;
+                        break;
+                    case GOLD:
+                    case PLATINUM:
+                        background_color = laf.COLOR_WHITE;
+                        text_color = laf.COLOR_BLACK;
+                        break;
+                }
+
+                float w = laf.LINE_HEIGHT * 1.25f;
+                applet.fill(background_color);
+                applet.rect(x + 1, laf.LINE_HEIGHT * n + 1, w - 2, laf.LINE_HEIGHT - 2);
+                applet.fill(text_color);
+                applet.textAlign(CENTER);
+                applet.text(carNumber, x + w / 2, laf.LINE_HEIGHT * (n + 0.5f));
+                x += w;
             }
+            n++;
         }
-        LookAndFeel.drawScrollBar(applet, accidents.size(), visibleLines, scroll);
+
+        //Draw scroll bar.
+        float barWidth = 0.4f * laf.LINE_HEIGHT;
+        float maxBarHeight = height - laf.LINE_HEIGHT;
+        float itemHeight = maxBarHeight / Math.max(incidents.size(), 1);
+        float barHeight = Math.min(itemHeight * (visibleRows - 1), maxBarHeight);
+        applet.fill(laf.COLOR_DARK_DARK_GRAY);
+        applet.rect(0, laf.LINE_HEIGHT, barWidth, maxBarHeight);
+        applet.fill(laf.COLOR_RED);
+        applet.rect(barWidth * 0.2f, laf.LINE_HEIGHT + scroll * itemHeight, barWidth * 0.6f, barHeight);
     }
 
     @Override
     public void mouseWheel(int count) {
-        scroll += count;
+        int min = 0;
+        int max = Math.max(incidents.size() - (visibleRows - 1), 0);
+        scroll = Math.min(Math.max(scroll + count, min), max);
+        applet.forceRedraw();
     }
+
+    @Override
+    public void resize(int w, int h) {
+        super.resize(w, h);
+        calculateColumnWidths();
+        visibleRows = h / LookAndFeel.get().LINE_HEIGHT;
+    }
+
+    private void drawCellBackground(Column c, int y, int color) {
+        float lineHeight = LookAndFeel.get().LINE_HEIGHT;
+        applet.fill(color);
+        applet.rect(c.xOffset * lineHeight + 1, y * lineHeight + 1,
+                c.size * lineHeight - 2, lineHeight - 2);
+    }
+
+    private void drawCellText(String text, Column c, int y, int color) {
+        float lineHeight = LookAndFeel.get().LINE_HEIGHT;
+
+        float xoffset = c.xOffset;
+        switch (c.alignment) {
+            case LEFT:
+                xoffset += 0.2f;
+                break;
+            case RIGHT:
+                xoffset += c.size - 0.2f;
+                break;
+            case CENTER:
+                xoffset += c.size / 2;
+                break;
+        }
+        applet.fill(color);
+        applet.textAlign(c.alignment, CENTER);
+        applet.text(text, xoffset * lineHeight, y * lineHeight + lineHeight / 2);
+    }
+
+    private void calculateColumnWidths() {
+        float lineHeight = LookAndFeel.get().LINE_HEIGHT;
+        float w = width / lineHeight;
+
+        float staticSize = 0;
+        int dynamicCount = 0;
+        List<Column> dynamicColumns = new LinkedList<>();
+        for (Column c : columns) {
+            if (!c.dynamicSize) {
+                staticSize += c.size;
+            } else {
+                dynamicColumns.add(c);
+                dynamicCount++;
+            }
+        }
+        final float dynamicSize = Math.max(w - staticSize, 0);
+        final int count = dynamicCount;
+        dynamicColumns.forEach(column -> column.size = Math.max(dynamicSize / count, column.minSize));
+
+        //calculate offset for each column.
+        float xOffset = 0;
+        for (Column c : columns) {
+            c.xOffset = xOffset;
+            xOffset += c.size;
+        }
+    }
+
+    private class Column {
+
+        public final String head;
+        public float size;
+        public float xOffset;
+        public float minSize;
+        public final boolean dynamicSize;
+        public final int alignment;
+
+        public Column(String head, float size, boolean dynamicSize, int alignment) {
+            this.head = head;
+            this.size = size;
+            this.minSize = size;
+            this.dynamicSize = dynamicSize;
+            this.alignment = alignment;
+        }
+
+    }
+
 }
