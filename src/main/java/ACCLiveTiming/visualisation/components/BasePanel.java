6650d8e83aaa4f894a8f14570811773faeb0243f
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-09-28 21:22:56 +0200

    Create the base panel.

---
+++ src/main/java/ACCLiveTiming/visualisation/components/BasePanel.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACCLiveTiming.visualisation.components;
+
+import ACCLiveTiming.client.BasicAccBroadcastingClient;
+import ACCLiveTiming.visualisation.LookAndFeel;
+import ACCLiveTiming.visualisation.gui.LPComponent;
+import ACCLiveTiming.visualisation.gui.LPContainer;
+import ACCLiveTiming.visualisation.gui.LPTabPanel;
+
+/**
+ *
+ * @author Leonard
+ */
+public class BasePanel extends LPContainer{
+    
+    private final LPComponent header;
+    private final LPComponent body;
+    
+    public BasePanel(BasicAccBroadcastingClient client){
+        header = new HeaderPanel(client);
+        addComponent(header);
+        
+        
+        LPTabPanel tabs = new LPTabPanel();
+        LPPanel l = new LPPanel();
+        l.setName("T1");
+        tabs.addTab(l);
+        l = new LPPanel();
+        l.setName("T2");
+        tabs.addTab(l);
+        l = new LPPanel();
+        l.setName("T3");
+        tabs.addTab(l);
+        body = tabs;
+        addComponent(body);
+    }
+    
+    @Override
+    public void onParentResize(int w, int h){
+        header.setSize(w, LookAndFeel.get().LINE_HEIGHT);
+        header.setPosition(0, 0);
+        body.setSize(w, h-LookAndFeel.get().LINE_HEIGHT);
+        body.setPosition(0, LookAndFeel.get().LINE_HEIGHT);
+        invalidate();
+    }
+    
+}
---
+++ src/main/java/ACCLiveTiming/visualisation/components/HeaderPanel.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACCLiveTiming.visualisation.components;
+
+import ACCLiveTiming.client.BasicAccBroadcastingClient;
+import ACCLiveTiming.client.SessionId;
+import ACCLiveTiming.utility.TimeUtils;
+import ACCLiveTiming.visualisation.LookAndFeel;
+import ACCLiveTiming.visualisation.gui.LPComponent;
+import processing.core.PApplet;
+import static processing.core.PConstants.CENTER;
+import static processing.core.PConstants.LEFT;
+import static processing.core.PConstants.RIGHT;
+
+/**
+ *
+ * @author Leonard
+ */
+public class HeaderPanel extends LPComponent {
+
+    private final BasicAccBroadcastingClient client;
+
+    public HeaderPanel(BasicAccBroadcastingClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public void draw() {
+        String sessionTimeLeft = TimeUtils.asDurationShort(client.getModel().getSessionInfo().getSessionEndTime());
+        String sessionName = sessionIdToString(client.getSessionId());
+
+        applet.fill(30);
+        applet.noStroke();
+        applet.rect(0, 0, getWidth(), getHeight());
+
+        String conId = "CON-ID: " + client.getModel().getConnectionID();
+        String packetsReceived = "Packets received: " + client.getPacketCount();
+
+        applet.fill(255);
+        applet.textAlign(LEFT, CENTER);
+        applet.textFont(LookAndFeel.get().FONT);
+        applet.text(conId, 10, LookAndFeel.get().LINE_HEIGHT * 0.5f);
+        applet.text(packetsReceived, 200, LookAndFeel.get().LINE_HEIGHT * 0.5f);
+
+        applet.textAlign(RIGHT, CENTER);
+        applet.text(sessionName, applet.width - 10, LookAndFeel.get().LINE_HEIGHT / 2f);
+        applet.text(sessionTimeLeft,
+                applet.width - applet.textWidth(sessionName) - 40,
+                LookAndFeel.get().LINE_HEIGHT / 2f);
+
+        applet.fill(0, 150, 0);
+        applet.rect(applet.width - applet.textWidth(sessionName) - 30,
+                LookAndFeel.get().LINE_HEIGHT * 0.1f,
+                10, LookAndFeel.get().LINE_HEIGHT * 0.8f);
+
+    }
+
+    private String sessionIdToString(SessionId sessionId) {
+        String result = "";
+        switch (sessionId.getType()) {
+            case PRACTICE:
+                result = "PRACTICE";
+                break;
+            case QUALIFYING:
+                result = "QUALIFYING";
+                break;
+            case RACE:
+                result = "RACE";
+                break;
+            default:
+                result = "NOT SUPPORTED";
+                break;
+        }
+        return result + " " + sessionId.getNumber();
+    }
+
+}
