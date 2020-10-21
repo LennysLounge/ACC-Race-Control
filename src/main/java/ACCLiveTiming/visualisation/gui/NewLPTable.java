11b3cf8b5c0dc61e1ba0f0351495f54f07dbabc6
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-10-21 16:38:23 +0200

    Add a new LPTable and Table Model

---
+++ src/main/java/ACCLiveTiming/visualisation/gui/NewLPTable.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACCLiveTiming.visualisation.gui;
+
+/**
+ *
+ * @author Leonard
+ */
+public class NewLPTable extends LPContainer {
+    
+    public void draw(){
+        applet.noFill();
+        applet.rect(0,0,getWidth(),getHeight());
+    }
+    
+}
