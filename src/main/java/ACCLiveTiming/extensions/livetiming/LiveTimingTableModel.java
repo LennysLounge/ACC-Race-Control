8eec256cf8aac0d9646b5c85b81fadc6209d8fac
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-10-30 01:41:49 +0100

    add the laptime and delta column with cell renderers.

--- src/main/java/ACCLiveTiming/extensions/livetiming/LiveTimingTableModel.java
+++ src/main/java/ACCLiveTiming/extensions/livetiming/LiveTimingTableModel.java
@@ -6,12 +6,15 @@
 package ACCLiveTiming.extensions.livetiming;
 
 import ACCLiveTiming.networking.data.CarInfo;
+import ACCLiveTiming.networking.data.LapInfo;
 import ACCLiveTiming.networking.enums.CarLocation;
 import ACCLiveTiming.networking.enums.DriverCategory;
 import static ACCLiveTiming.networking.enums.DriverCategory.BRONZE;
 import static ACCLiveTiming.networking.enums.DriverCategory.GOLD;
 import static ACCLiveTiming.networking.enums.DriverCategory.PLATINUM;
 import static ACCLiveTiming.networking.enums.DriverCategory.SILVER;
+import ACCLiveTiming.networking.enums.LapType;
+import ACCLiveTiming.utility.TimeUtils;
 import ACCLiveTiming.visualisation.LookAndFeel;
 import ACCLiveTiming.visualisation.gui.LPTableColumn;
 import ACCLiveTiming.visualisation.gui.LPTable;
@@ -26,6 +29,92 @@ import static processing.core.PConstants.CENTER;
  * @author Leonard
  */
 public class LiveTimingTableModel extends TableModel {
+
+    /**
+     * Ordered list of car entries
+     */
+    private List<CarInfo> entries = new LinkedList<>();
+    /**
+     * Car id of the focused car.
+     */
+    private int focusedCarId = -1;
+
+    @Override
+    public int getRowCount() {
+        return entries.size();
+    }
+
+    @Override
+    public LPTableColumn[] getColumns() {
+        return new LPTableColumn[]{
+            new LPTableColumn("P")
+            .setMinWidth(40)
+            .setMaxWidth(40)
+            .setCellRenderer(positionRenderer),
+            new LPTableColumn("Name")
+            .setMaxWidth(240),
+            new LPTableColumn("")
+            .setMaxWidth(16)
+            .setMinWidth(16)
+            .setCellRenderer(pitRenderer),
+            new LPTableColumn("#")
+            .setMinWidth(60)
+            .setMaxWidth(60)
+            .setCellRenderer(carNumberRenderer),
+            new LPTableColumn("Lap")
+            .setCellRenderer(lapTimeRenderer),
+            new LPTableColumn("Delta")
+            .setCellRenderer(deltaRenderer)
+        };
+    }
+
+    @Override
+    public Object getValueAt(int column, int row) {
+        CarInfo car = entries.get(row);
+        switch (column) {
+            case 0:
+                return new Tuple(
+                        car.getRealtime().getPosition(),
+                        car.getCarId() == focusedCarId
+                );
+            case 1:
+                String firstname = car.getDriver().getFirstName();
+                String lastname = car.getDriver().getLastName();
+                firstname = firstname.substring(0, Math.min(firstname.length(), 1));
+                return String.format("%s. %s", firstname, lastname);
+            case 2:
+                return car.getRealtime().getLocation() != CarLocation.TRACK;
+            case 3:
+                return new Tuple(
+                        car.getCarNumber(),
+                        car.getDriver().getCategory()
+                );
+            case 4:
+                return car;
+            case 5:
+                return car;
+        }
+        return "-";
+    }
+
+    public void setEntries(List<CarInfo> entries) {
+        this.entries = entries;
+    }
+
+    public void setFocusedCarId(int carId) {
+        focusedCarId = carId;
+    }
+
+    private class Tuple {
+
+        public Object left;
+        public Object right;
+
+        public Tuple(Object left, Object right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
 
     private final LPTable.CellRenderer positionRenderer = (
             PApplet applet,
@@ -44,7 +133,7 @@ public class LiveTimingTableModel extends TableModel {
             fgColor = LookAndFeel.COLOR_BLACK;
         }
         applet.fill(bgColor);
-        applet.rect(1, 1, width-2, height-2);
+        applet.rect(1, 1, width - 2, height - 2);
         applet.fill(fgColor);
         applet.textAlign(CENTER, CENTER);
         applet.text(String.valueOf(t.left),
@@ -63,7 +152,7 @@ public class LiveTimingTableModel extends TableModel {
         if (isInPits) {
             applet.noStroke();
             applet.fill(LookAndFeel.COLOR_WHITE);
-            applet.rect(1, 1, width-2, height-2);
+            applet.rect(1, 1, width - 2, height - 2);
             applet.fill(0);
             applet.textAlign(CENTER, CENTER);
             applet.textSize(12);
@@ -100,91 +189,61 @@ public class LiveTimingTableModel extends TableModel {
         }
         applet.noStroke();
         applet.fill(backColor);
-        applet.rect(1, 1, width-2, height-2);
+        applet.rect(1, 1, width - 2, height - 2);
         applet.fill(frontColor);
         applet.textAlign(CENTER, CENTER);
         applet.text(String.valueOf(t.left), width / 2f, height / 2f);
     };
 
-    /**
-     * Ordered list of car entries
-     */
-    private List<CarInfo> entries = new LinkedList<>();
-    /**
-     * Car id of the focused car.
-     */
-    private int focusedCarId = -1;
-
-    @Override
-    public int getRowCount() {
-        return entries.size();
-    }
-
-    @Override
-    public LPTableColumn[] getColumns() {
-        return new LPTableColumn[]{
-            new LPTableColumn("P")
-            .setMinWidth(40)
-            .setMaxWidth(40)
-            .setCellRenderer(positionRenderer),
-            new LPTableColumn("Name")
-            .setMaxWidth(240),
-            new LPTableColumn("")
-            .setMaxWidth(16)
-            .setMinWidth(16)
-            .setCellRenderer(pitRenderer),
-            new LPTableColumn("#")
-            .setMinWidth(60)
-            .setMaxWidth(60)
-            .setCellRenderer(carNumberRenderer),
-            new LPTableColumn("lap")
-        };
-    }
-
-    @Override
-    public Object getValueAt(int column, int row) {
-        CarInfo car = entries.get(row);
-        switch (column) {
-            case 0:
-                return new Tuple(
-                        car.getRealtime().getPosition(),
-                        car.getCarId() == focusedCarId
-                );
-            case 1:
-                String firstname = car.getDriver().getFirstName();
-                String lastname = car.getDriver().getLastName();
-                firstname = firstname.substring(0, Math.min(firstname.length(), 1));
-                return String.format("%s. %s", firstname, lastname);
-            case 2:
-                return car.getRealtime().getLocation() != CarLocation.TRACK;
-            case 3:
-                return new Tuple(
-                        car.getCarNumber(),
-                        car.getDriver().getCategory()
-                );
-            case 4:
-                return "--:--.--";
+    private final LPTable.CellRenderer lapTimeRenderer = (
+            PApplet applet,
+            Object object,
+            boolean isSelected,
+            boolean isMouseOverRow,
+            boolean isMouseOverColumn,
+            float width,
+            float height) -> {
+        CarInfo car = (CarInfo) object;
+        LapInfo currentLap = car.getRealtime().getCurrentLap();
+        String text = "";
+        if (car.getRealtime().getLocation() == CarLocation.TRACK
+                && currentLap.getType() == LapType.REGULAR) {
+            applet.fill(LookAndFeel.COLOR_WHITE);
+            if (currentLap.getIsInvalid()) {
+                applet.fill(LookAndFeel.COLOR_RED);
+            }
+            text = TimeUtils.asLapTime(currentLap.getLapTimeMS());
+        } else if (car.getRealtime().getLocation() == CarLocation.TRACK) {
+            applet.fill(0, 0, 0, 75);
+            text = currentLap.getType().name();
         }
-        return "-";
-    }
+        applet.textAlign(CENTER, CENTER);
+        applet.text(text, width / 2, height / 2);
+    };
 
-    public void setEntries(List<CarInfo> entries) {
-        this.entries = entries;
-    }
+    private final LPTable.CellRenderer deltaRenderer = (
+            PApplet applet,
+            Object object,
+            boolean isSelected,
+            boolean isMouseOverRow,
+            boolean isMouseOverColumn,
+            float width,
+            float height) -> {
+        CarInfo car = (CarInfo) object;
+        LapInfo currentLap = car.getRealtime().getCurrentLap();
 
-    public void setFocusedCarId(int carId) {
-        focusedCarId = carId;
-    }
+        String text = "";
+        if (car.getRealtime().getLocation() == CarLocation.TRACK
+                && currentLap.getType() == LapType.REGULAR) {
 
-    private class Tuple {
-
-        public Object left;
-        public Object right;
-
-        public Tuple(Object left, Object right) {
-            this.left = left;
-            this.right = right;
+            applet.fill(LookAndFeel.COLOR_RACE);
+            if (car.getRealtime().getDelta() > 0) {
+                applet.fill(LookAndFeel.COLOR_RED);
+            }
+            text = TimeUtils.asDelta(car.getRealtime().getDelta());
+            applet.textAlign(CENTER, CENTER);
+            applet.text(text, width / 2, height / 2);
         }
-    }
+    };
 
 }
