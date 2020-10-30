9a6ee356c1676f7e1fad28ffa40e127bcf0acd55
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-10-30 23:32:14 +0100

    Turn this project into a multi project build.
    Add a extension test project.

---
+++ extension/src/main/java/extension/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extension;
+
+/**
+ *
+ * @author Leonard
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("extension");
+    }
+}
