63473af16da6f32bb4f556903ac7fc9244372e71
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-12-14 22:50:08 +0100

    Add a listener interface.

---
+++ Monitor/src/main/java/ACCLiveTiming/Monitor/networking/AccBroadcastingClientListener.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACCLiveTiming.Monitor.networking;
+
+import ACCLiveTiming.monitor.networking.data.BroadcastingEvent;
+import ACCLiveTiming.monitor.networking.data.CarInfo;
+import ACCLiveTiming.monitor.networking.data.RealtimeInfo;
+import ACCLiveTiming.monitor.networking.data.SessionInfo;
+import ACCLiveTiming.monitor.networking.data.TrackInfo;
+import java.util.List;
+
+/**
+ *
+ * @author Leonard
+ */
+public interface AccBroadcastingClientListener {
+
+    public void onRegistrationResult(int connectionID, boolean success, boolean readOnly, String message);
+
+    public void onRealtimeUpdate(SessionInfo sessionInfo);
+
+    public void onRealtimeCarUpdate(RealtimeInfo info);
+
+    public void onEntryListUpdate(List<Integer> carIds);
+
+    public void onTrackData(TrackInfo info);
+
+    public void onEntryListCarUpdate(CarInfo carInfo);
+
+    public void onBroadcastingEvent(BroadcastingEvent event);
+
+    public void afterPacketReceived(byte type);
+}
