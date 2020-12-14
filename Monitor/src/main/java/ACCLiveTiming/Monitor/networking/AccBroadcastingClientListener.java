a48bf1b27ec3e3722ed021656395e5367da03522
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-12-14 22:50:55 +0100

    lowercase packet name

--- Monitor/src/main/java/ACCLiveTiming/Monitor/networking/AccBroadcastingClientListener.java
+++ Monitor/src/main/java/ACCLiveTiming/Monitor/networking/AccBroadcastingClientListener.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ACCLiveTiming.Monitor.networking;
+package ACCLiveTiming.monitor.networking;
 
 import ACCLiveTiming.monitor.networking.data.BroadcastingEvent;
 import ACCLiveTiming.monitor.networking.data.CarInfo;
