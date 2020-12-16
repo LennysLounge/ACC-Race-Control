1a9783a9212997463cce7bdbdda8981933c6acfc
Leonard Schüngel <schuengel.leonard@googlemail.com>
2020-12-16 16:26:39 +0100

    Turn the client extension into an interface that provides a panel.

--- Monitor/src/main/java/base/screen/extensions/AccClientExtension.java
+++ Monitor/src/main/java/base/screen/extensions/AccClientExtension.java
@@ -5,40 +5,16 @@
  */
 package base.screen.extensions;
 
-import base.screen.networking.AccBroadcastingClient;
 import base.screen.visualisation.gui.LPContainer;
 
 /**
  *
  * @author Leonard
  */
-public class AccClientExtension {
-    /**
-     * Base client for this extension.
-     */
-    protected AccBroadcastingClient client;
-    /**
-     * The panel that is attached to this extension.
-     * If null then no panel is attached.
-     */
-    protected LPContainer panel = null;
-    /**
-     * Returns true if there is an extension Panel for this extension.
-     * @return If there is an extension.
-     */
-    public boolean hasPanel(){
-        return (panel != null);
-    }
+public interface AccClientExtension {
     /**
      * Returns the attached panel for this extension.
      * @return The attached panel.
      */
-    public LPContainer getPanel(){
-        return panel;
-    }
-    
-    public void setClient(AccBroadcastingClient client){
-        this.client = client;
-    }
-      
+    public LPContainer getPanel();
 }
