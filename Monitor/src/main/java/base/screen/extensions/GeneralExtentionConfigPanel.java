01b2f8c67dec32cf2a589bd611f5dca0de35741e
Leonard Schüngel <schuengel.leonard@googlemail.com>
2021-04-16 19:22:39 +0200

    better formatting.

--- Monitor/src/main/java/base/screen/extensions/GeneralExtentionConfigPanel.java
+++ Monitor/src/main/java/base/screen/extensions/GeneralExtentionConfigPanel.java
@@ -22,13 +22,13 @@ public class GeneralExtentionConfigPanel
 
     private final LPCheckBox liveTimingCheckBox = new LPCheckBox();
     private final LPLabel liveTimingLabel = new LPLabel("Enable live timing screen");
-    
+
     private final LPCheckBox incidentLogCheckBox = new LPCheckBox();
     private final LPLabel incidentLogLabel = new LPLabel("Enable incidents screen");
-    
+
     private final LPCheckBox loggingCheckBox = new LPCheckBox();
     private final LPLabel loggingLabel = new LPLabel("Enable logging screen");
-    
+
     private final LPCheckBox trackMapCheckBox = new LPCheckBox();
     private final LPLabel trackMapLabel = new LPLabel("Enable track map");
 
@@ -50,43 +50,37 @@ public class GeneralExtentionConfigPanel
         liveTimingCheckBox.setSelected(true);
         addComponent(liveTimingCheckBox);
         liveTimingLabel.setPosition(60, LINE_HEIGHT);
-        liveTimingLabel.setSize(300,LINE_HEIGHT);
+        liveTimingLabel.setSize(300, LINE_HEIGHT);
         addComponent(liveTimingLabel);
-        
-        incidentLogCheckBox.setPosition(20, LINE_HEIGHT*2 + (LINE_HEIGHT - TEXT_SIZE) / 2);
+
+        incidentLogCheckBox.setPosition(20, LINE_HEIGHT * 2 + (LINE_HEIGHT - TEXT_SIZE) / 2);
         incidentLogCheckBox.setSelected(true);
         addComponent(incidentLogCheckBox);
-        incidentLogLabel.setPosition(60, LINE_HEIGHT*2);
-        incidentLogLabel.setSize(300,LINE_HEIGHT);
+        incidentLogLabel.setPosition(60, LINE_HEIGHT * 2);
+        incidentLogLabel.setSize(300, LINE_HEIGHT);
         addComponent(incidentLogLabel);
-        
-        loggingCheckBox.setPosition(20, LINE_HEIGHT*3 + (LINE_HEIGHT - TEXT_SIZE) / 2);
+
+        loggingCheckBox.setPosition(20, LINE_HEIGHT * 3 + (LINE_HEIGHT - TEXT_SIZE) / 2);
         loggingCheckBox.setSelected(true);
         addComponent(loggingCheckBox);
-        loggingLabel.setPosition(60, LINE_HEIGHT*3);
-        loggingLabel.setSize(300,LINE_HEIGHT);
+        loggingLabel.setPosition(60, LINE_HEIGHT * 3);
+        loggingLabel.setSize(300, LINE_HEIGHT);
         addComponent(loggingLabel);
-        
-        trackMapCheckBox.setPosition(20, LINE_HEIGHT*4 + (LINE_HEIGHT - TEXT_SIZE) / 2);
+
+        trackMapCheckBox.setPosition(20, LINE_HEIGHT * 4 + (LINE_HEIGHT - TEXT_SIZE) / 2);
         trackMapCheckBox.setSelected(false);
         trackMapCheckBox.setEnabled(false);
         addComponent(trackMapCheckBox);
-        trackMapLabel.setPosition(60, LINE_HEIGHT*4);
-        trackMapLabel.setSize(300,LINE_HEIGHT);
+        trackMapLabel.setPosition(60, LINE_HEIGHT * 4);
+        trackMapLabel.setSize(300, LINE_HEIGHT);
         addComponent(trackMapLabel);
     }
-    
+
     @Override
-    public void onEnabled(){
-        if(isEnabled()){
-            liveTimingCheckBox.setEnabled(true);
-            loggingCheckBox.setEnabled(true);
-            incidentLogCheckBox.setEnabled(true);
-        }else{
-            liveTimingCheckBox.setEnabled(false);
-            loggingCheckBox.setEnabled(false);
-            incidentLogCheckBox.setEnabled(false);
-        }
+    public void onEnabled() {
+        liveTimingCheckBox.setEnabled(isEnabled());
+        loggingCheckBox.setEnabled(isEnabled());
+        incidentLogCheckBox.setEnabled(isEnabled());
         invalidate();
     }
 
@@ -96,16 +90,16 @@ public class GeneralExtentionConfigPanel
         }
         return instance;
     }
-    
-    public boolean isLiveTimingEnabled(){
+
+    public boolean isLiveTimingEnabled() {
         return liveTimingCheckBox.isSelected();
     }
-    
-    public boolean isIncidentLogEnabled(){
+
+    public boolean isIncidentLogEnabled() {
         return incidentLogCheckBox.isSelected();
     }
-    
-    public boolean isLoggingEnabled(){
+
+    public boolean isLoggingEnabled() {
         return loggingCheckBox.isSelected();
     }
 
