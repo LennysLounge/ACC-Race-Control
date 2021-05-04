a874d9a91b5fe9a83b78094f34c76e679379ac2c
Leonard Schüngel <schuengel.leonard@googlemail.com>
2021-05-04 22:36:06 +0200

    Move bat back to main folder

---
+++ Monitor/src/dist/Start.bat
@@ -0,0 +1,2 @@
+@echo off
+start /b ./jre/bin/javaw.exe -splash:bin/loading.png -cp .\lib\* base.screen.Main 
--- Monitor/src/dist/bin/Start.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start /b ./../jre/bin/javaw.exe -splash:loading.png -cp .\..\lib\* base.screen.Main 
