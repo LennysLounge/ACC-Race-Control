8613f36b454742bca9f933fa92185f4ae2bbf99d
Leonard Schüngel <schuengel.leonard@googlemail.com>
2021-09-08 18:42:19 +0200

    Avoid a null pointer exception here.

--- Monitor/src/main/java/racecontrol/app/HeaderPanel.java
+++ Monitor/src/main/java/racecontrol/app/HeaderPanel.java
@@ -20,6 +20,11 @@ import static racecontrol.LookAndFeel.fontRegular;
 import static processing.core.PConstants.CENTER;
 import static processing.core.PConstants.LEFT;
 import static processing.core.PConstants.RIGHT;
+import racecontrol.client.events.ConnectionClosed;
+import racecontrol.client.events.ConnectionOpened;
+import racecontrol.eventbus.Event;
+import racecontrol.eventbus.EventBus;
+import racecontrol.eventbus.EventListener;
 import racecontrol.lpgui.gui.LPContainer;
 
 /**
@@ -27,7 +32,8 @@ import racecontrol.lpgui.gui.LPContainer;
  * @author Leonard
  */
 public class HeaderPanel
-        extends LPContainer {
+        extends LPContainer
+        implements EventListener {
 
     private final AccBroadcastingClient client;
     /**
@@ -36,6 +42,7 @@ public class HeaderPanel
     private ReplayOffsetExtension replayOffsetExtension;
 
     public HeaderPanel() {
+        EventBus.register(this);
         this.client = AccBroadcastingClient.getClient();
     }
 
@@ -81,7 +88,7 @@ public class HeaderPanel
             applet.textAlign(RIGHT, CENTER);
             applet.textSize(TEXT_SIZE * 0.8f);
             float sessionNameWidth = applet.textWidth(sessionName);
-            applet.text(sessionName, getWidth()-10, y + LINE_HEIGHT * 0.5f);
+            applet.text(sessionName, getWidth() - 10, y + LINE_HEIGHT * 0.5f);
             applet.textFont(fontMedium());
             applet.textSize(TEXT_SIZE);
             applet.text(sessionTimeLeft,
@@ -120,5 +127,12 @@ public class HeaderPanel
                 break;
         }
         return result;
+    }
+
+    @Override
+    public void onEvent(Event e) {
+        if (e instanceof ConnectionOpened) {
+            replayOffsetExtension = client.getOrCreateExtension(ReplayOffsetExtension.class);
+        }
     }
 }
