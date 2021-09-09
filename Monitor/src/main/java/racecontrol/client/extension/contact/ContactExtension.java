b883d8b2f3641129b4a54643028e1f78d4ccb077
Leonard Schüngel <schuengel.leonard@googlemail.com>
2021-09-09 22:50:41 +0200

    remove unused methods.
    Save client as field.

--- Monitor/src/main/java/racecontrol/client/extension/contact/ContactExtension.java
+++ Monitor/src/main/java/racecontrol/client/extension/contact/ContactExtension.java
@@ -16,8 +16,6 @@ import racecontrol.utility.TimeUtils;
 import racecontrol.extensions.replayoffset.ReplayOffsetExtension;
 import racecontrol.client.AccBroadcastingClient;
 import java.util.logging.Logger;
-import static racecontrol.client.AccBroadcastingClient.getClient;
-import racecontrol.client.data.CarInfo;
 import racecontrol.eventbus.EventListener;
 import racecontrol.logging.UILogger;
 
@@ -33,11 +31,14 @@ public class ContactExtension
      * Singelton instance.
      */
     private static ContactExtension instance = null;
-
     /**
      * This classes logger.
      */
     private static final Logger LOG = Logger.getLogger(ContactExtension.class.getName());
+    /**
+     * Reference to the connection client.
+     */
+    private final AccBroadcastingClient client;
     /**
      * Last accident that is waiting to be commited.
      */
@@ -56,7 +57,8 @@ public class ContactExtension
 
     private ContactExtension() {
         EventBus.register(this);
-        replayOffsetExtension = AccBroadcastingClient.getClient().getOrCreateExtension(ReplayOffsetExtension.class);
+        client = AccBroadcastingClient.getClient();
+        replayOffsetExtension = client.getOrCreateExtension(ReplayOffsetExtension.class);
     }
 
     @Override
@@ -82,95 +84,38 @@ public class ContactExtension
     }
 
     public void onAccident(BroadcastingEvent event) {
-        float sessionTime = getClient().getModel().getSessionInfo().getSessionTime();
-        String logMessage = "Accident: #" + getClient().getModel().getCar(event.getCarId()).getCarNumber()
+        float sessionTime = client.getModel().getSessionInfo().getSessionTime();
+        int replaytime = replayOffsetExtension.getReplayTimeFromSessionTime((int)sessionTime);
+        String logMessage = "Contact: #" + client.getModel().getCar(event.getCarId()).getCarNumber()
                 + "\t\t" + TimeUtils.asDuration(sessionTime)
-                + "\t" + TimeUtils.asDuration(replayOffsetExtension.getReplayTimeFromConnectionTime(event.getTimeMs()));
+                + "\t" + TimeUtils.asDuration(replaytime);
         UILogger.log(logMessage);
         LOG.info(logMessage);
 
-        SessionId sessionId = getClient().getSessionId();
+        SessionId sessionId = client.getSessionId();
         if (stagedAccident == null) {
             stagedAccident = new ContactInfo(sessionTime,
-                    replayOffsetExtension.isReplayTimeKnown() ? replayOffsetExtension.getReplayTimeFromSessionTime((int) sessionTime) : 0,
-                    getClient().getModel().getCar(event.getCarId()),
+                    replaytime,
+                    client.getModel().getCar(event.getCarId()),
                     sessionId);
         } else {
             float timeDif = stagedAccident.getSessionLatestTime() - sessionTime;
             if (timeDif > 1000) {
                 commitAccident(stagedAccident);
                 stagedAccident = new ContactInfo(sessionTime,
-                        replayOffsetExtension.isReplayTimeKnown() ? replayOffsetExtension.getReplayTimeFromSessionTime((int) sessionTime) : 0,
-                        getClient().getModel().getCar(event.getCarId()),
+                        replaytime,
+                        client.getModel().getCar(event.getCarId()),
                         sessionId);
             } else {
                 stagedAccident = stagedAccident.addCar(sessionTime,
-                        getClient().getModel().getCar(event.getCarId()),
+                        client.getModel().getCar(event.getCarId()),
                         System.currentTimeMillis());
             }
         }
     }
 
-    public void addEmptyAccident() {
-        float sessionTime = getClient().getModel().getSessionInfo().getSessionTime();
-        commitAccident(new ContactInfo(sessionTime,
-                replayOffsetExtension.isReplayTimeKnown() ? replayOffsetExtension.getReplayTimeFromSessionTime((int) sessionTime) : 0,
-                getClient().getSessionId()));
-    }
-
     private void commitAccident(ContactInfo a) {
         EventBus.publish(new ContactEvent(a));
-    }
-
-    public void findReplayOffset() {
-        replayOffsetExtension.findSessionChange();
-    }
-
-    public void createDummyIncident() {
-        if (getClient().getModel().getCarsInfo().isEmpty()) {
-            return;
-        }
-        LOG.info("Create dummy accident.");
-        int nCars = (int) Math.floor(Math.random() * Math.min(6, getClient().getModel().getCarsInfo().size()) + 1);
-        float sessionTime = getClient().getModel().getSessionInfo().getSessionTime();
-        ContactInfo incident = new ContactInfo(
-                sessionTime,
-                replayOffsetExtension.getReplayTimeFromSessionTime((int) sessionTime),
-                getClient().getSessionId());
-        for (int i = 0; i < nCars; i++) {
-            incident = incident.addCar(
-                    sessionTime,
-                    getRandomCar(),
-                    0);
-        }
-        commitAccident(incident);
-    }
-
-    private CarInfo getRandomCar() {
-        int r = (int) Math.floor(Math.random() * getClient().getModel().getCarsInfo().size());
-        int i = 0;
-        for (CarInfo car : getClient().getModel().getCarsInfo().values()) {
-            if (i++ == r) {
-                return car;
-            }
-        }
-        return null;
-    }
-
-    public void focusOnCar(int carId) {
-        LOG.info("Setting focus on car " + carId);
-        getClient().sendChangeFocusRequest(carId);
-    }
-
-    public void startAccidentReplay(ContactInfo incident, int seconds) {
-        LOG.info("Starting instant replay for incident for " + seconds);
-        getClient().sendInstantReplayRequestWithCamera(
-                incident.getSessionEarliestTime() - seconds * 1000f - 5000,
-                seconds + 5,
-                getClient().getModel().getSessionInfo().getFocusedCarIndex(),
-                getClient().getModel().getSessionInfo().getActiveCameraSet(),
-                getClient().getModel().getSessionInfo().getActiveCamera()
-        );
     }
 
 }
