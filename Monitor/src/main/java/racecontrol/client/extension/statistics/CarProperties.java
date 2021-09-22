99a238254e65ac2f6878a6a78021b83b5dbc2da2
Leonard Schüngel <schuengel.leonard@googlemail.com>
2021-09-22 18:09:27 +0200

    Avoid a unchecked cast warning here
    Fix license

--- Monitor/src/main/java/racecontrol/client/extension/statistics/CarProperties.java
+++ Monitor/src/main/java/racecontrol/client/extension/statistics/CarProperties.java
@@ -15,43 +15,43 @@ import racecontrol.client.extension.statistics.WritableCarStatistics.Key;
  */
 public interface CarProperties {
 
-    public final Key<Integer> CAR_ID = new Key(Integer.class, 0);
+    public final Key<Integer> CAR_ID = new Key<>(Integer.class, 0);
 
     // Identity
-    public final Key<String> FIRSTNAME = new Key(String.class, "");
-    public final Key<String> SURNAME = new Key(String.class, "");
-    public final Key<String> FULL_NAME = new Key(String.class, "");
-    public final Key<String> NAME = new Key(String.class, "");
-    public final Key<String> SHORT_NAME = new Key(String.class, "");
-    public final Key<Integer> CAR_NUMBER = new Key(Integer.class, 0);
-    public final Key<Byte> CAR_MODEL = new Key(Byte.class, (byte) 0);
-    public final Key<DriverCategory> CATEGORY = new Key(DriverCategory.class, DriverCategory.BRONZE);
+    public final Key<String> FIRSTNAME = new Key<>(String.class, "");
+    public final Key<String> SURNAME = new Key<>(String.class, "");
+    public final Key<String> FULL_NAME = new Key<>(String.class, "");
+    public final Key<String> NAME = new Key<>(String.class, "");
+    public final Key<String> SHORT_NAME = new Key<>(String.class, "");
+    public final Key<Integer> CAR_NUMBER = new Key<>(Integer.class, 0);
+    public final Key<Byte> CAR_MODEL = new Key<>(Byte.class, (byte) 0);
+    public final Key<DriverCategory> CATEGORY = new Key<>(DriverCategory.class, DriverCategory.BRONZE);
     // Laps
-    public final Key<Integer> CURRENT_LAP_TIME = new Key(Integer.class, 0);
-    public final Key<Integer> LAST_LAP_TIME = new Key(Integer.class, 0);
-    public final Key<Integer> BEST_LAP_TIME = new Key(Integer.class, 0);
-    public final Key<Integer> DELTA = new Key(Integer.class, 0);
-    public final Key<Integer> GAP_TO_SESSION_BEST_LAP = new Key(Integer.class, 0);
-    public final Key<Boolean> IS_LAP_INVALID = new Key(Boolean.class, false);
-    public final Key<Integer> LAP_COUNT = new Key(Integer.class, 0);
-    public final Key<Integer> LAP_TIME_GAP_TO_SESSION_BEST = new Key(Integer.class, 0);
+    public final Key<Integer> CURRENT_LAP_TIME = new Key<>(Integer.class, 0);
+    public final Key<Integer> LAST_LAP_TIME = new Key<>(Integer.class, 0);
+    public final Key<Integer> BEST_LAP_TIME = new Key<>(Integer.class, 0);
+    public final Key<Integer> DELTA = new Key<>(Integer.class, 0);
+    public final Key<Integer> GAP_TO_SESSION_BEST_LAP = new Key<>(Integer.class, 0);
+    public final Key<Boolean> IS_LAP_INVALID = new Key<>(Boolean.class, false);
+    public final Key<Integer> LAP_COUNT = new Key<>(Integer.class, 0);
+    public final Key<Integer> LAP_TIME_GAP_TO_SESSION_BEST = new Key<>(Integer.class, 0);
     // Sectors
-    public final Key<Integer> LAST_SECTOR_ONE = new Key(Integer.class, 0);
-    public final Key<Integer> LAST_SECTOR_TWO = new Key(Integer.class, 0);
-    public final Key<Integer> LAST_SECTOR_THREE = new Key(Integer.class, 0);
-    public final Key<Integer> BEST_SECTOR_ONE = new Key(Integer.class, 0);
-    public final Key<Integer> BEST_SECTOR_TWO = new Key(Integer.class, 0);
-    public final Key<Integer> BEST_SECTOR_THREE = new Key(Integer.class, 0);
+    public final Key<Integer> LAST_SECTOR_ONE = new Key<>(Integer.class, 0);
+    public final Key<Integer> LAST_SECTOR_TWO = new Key<>(Integer.class, 0);
+    public final Key<Integer> LAST_SECTOR_THREE = new Key<>(Integer.class, 0);
+    public final Key<Integer> BEST_SECTOR_ONE = new Key<>(Integer.class, 0);
+    public final Key<Integer> BEST_SECTOR_TWO = new Key<>(Integer.class, 0);
+    public final Key<Integer> BEST_SECTOR_THREE = new Key<>(Integer.class, 0);
     // Status
-    public final Key<Integer> POSITION = new Key(Integer.class, 0);
-    public final Key<Integer> CUP_POSITION = new Key(Integer.class, 0);
-    public final Key<Boolean> IS_IN_PITS = new Key(Boolean.class, false);
-    public final Key<CarLocation> CAR_LOCATION = new Key(CarLocation.class, CarLocation.NONE);
+    public final Key<Integer> POSITION = new Key<>(Integer.class, 0);
+    public final Key<Integer> CUP_POSITION = new Key<>(Integer.class, 0);
+    public final Key<Boolean> IS_IN_PITS = new Key<>(Boolean.class, false);
+    public final Key<CarLocation> CAR_LOCATION = new Key<>(CarLocation.class, CarLocation.NONE);
 
     // Session
-    public final Key<Integer> SESSION_BEST_LAP_TIME = new Key(Integer.class, 0);
-    public final Key<Integer> SESSION_BEST_SECTOR_ONE = new Key(Integer.class, 0);
-    public final Key<Integer> SESSION_BEST_SECTOR_TWO = new Key(Integer.class, 0);
-    public final Key<Integer> SESSION_BEST_SECTOR_THREE = new Key(Integer.class, 0);
+    public final Key<Integer> SESSION_BEST_LAP_TIME = new Key<>(Integer.class, 0);
+    public final Key<Integer> SESSION_BEST_SECTOR_ONE = new Key<>(Integer.class, 0);
+    public final Key<Integer> SESSION_BEST_SECTOR_TWO = new Key<>(Integer.class, 0);
+    public final Key<Integer> SESSION_BEST_SECTOR_THREE = new Key<>(Integer.class, 0);
 
 }
