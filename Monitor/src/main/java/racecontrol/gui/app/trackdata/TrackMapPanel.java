f244a2d630e889c201024c99bec047646e445bf3
Leonard Schüngel <schuengel.leonard@googlemail.com>
2021-10-02 14:50:09 +0200

    Add sector lines and speedtrap to the trackmap

--- Monitor/src/main/java/racecontrol/gui/app/trackdata/TrackMapPanel.java
+++ Monitor/src/main/java/racecontrol/gui/app/trackdata/TrackMapPanel.java
@@ -78,6 +78,63 @@ public class TrackMapPanel
         applet.beginShape();
         trackMap.forEach(vec -> applet.curveVertex(vec.x + 20, vec.y + 20));
         applet.endShape(CLOSE);
+
+        // draw sector one lines
+        int lower = (int) Math.floor(trackData.getSectorOneLine() * trackMap.size());
+        int upper = ((lower + 1) % trackMap.size());
+        float t = trackData.getSectorOneLine() * trackMap.size() % 1;
+        PVector p = trackMap.get(lower).copy().mult(1 - t).add(
+                trackMap.get(upper).copy().mult(t)
+        );
+        PVector dir = trackMap.get(upper).copy().sub(trackMap.get(lower)).normalize();
+        dir = dir.set(-dir.y, dir.x);
+        applet.fill(255);
+        applet.line(p.x - dir.x * 15 + 20, p.y - dir.y * 15 + 20,
+                p.x + dir.x * 15 + 20, p.y + dir.y * 15 + 20);
+        applet.text("S1", p.x - dir.x * 30 + 20, p.y - dir.y * 30 + 20);
+
+        // sector two
+        lower = (int) Math.floor(trackData.getSectorTwoLine() * trackMap.size()) % trackMap.size();
+        upper = ((lower + 1) % trackMap.size());
+        t = trackData.getSectorTwoLine() * trackMap.size() % 1;
+        p = trackMap.get(lower).copy().mult(1 - t).add(
+                trackMap.get(upper).copy().mult(t)
+        );
+        dir = trackMap.get(upper).copy().sub(trackMap.get(lower)).normalize();
+        dir = dir.set(-dir.y, dir.x);
+        applet.fill(255);
+        applet.line(p.x - dir.x * 15 + 20, p.y - dir.y * 15 + 20,
+                p.x + dir.x * 15 + 20, p.y + dir.y * 15 + 20);
+        applet.text("S2", p.x - dir.x * 30 + 20, p.y - dir.y * 30 + 20);
+
+        // sector three
+        lower = (int) Math.floor(trackData.getSectorThreeLine() * trackMap.size()) % trackMap.size();
+        upper = ((lower + 1) % trackMap.size());
+        t = trackData.getSectorThreeLine() * trackMap.size() % 1;
+        p = trackMap.get(lower).copy().mult(1 - t).add(
+                trackMap.get(upper).copy().mult(t)
+        );
+        dir = trackMap.get(upper).copy().sub(trackMap.get(lower)).normalize();
+        dir = dir.set(-dir.y, dir.x);
+        applet.fill(255);
+        applet.line(p.x - dir.x * 15 + 20, p.y - dir.y * 15 + 20,
+                p.x + dir.x * 15 + 20, p.y + dir.y * 15 + 20);
+        applet.text("S3", p.x - dir.x * 30 + 20, p.y - dir.y * 30 + 20);
+
+        // speed trap
+        lower = (int) Math.floor(trackData.getSpeedTrapLine() * trackMap.size()) % trackMap.size();
+        upper = ((lower + 1) % trackMap.size());
+        t = trackData.getSpeedTrapLine() * trackMap.size() % 1;
+        p = trackMap.get(lower).copy().mult(1 - t).add(
+                trackMap.get(upper).copy().mult(t)
+        );
+        dir = trackMap.get(upper).copy().sub(trackMap.get(lower)).normalize();
+        dir = dir.set(-dir.y, dir.x);
+        applet.fill(255);
+        applet.line(p.x - dir.x * 15 + 20, p.y - dir.y * 15 + 20,
+                p.x + dir.x * 15 + 20, p.y + dir.y * 15 + 20);
+        applet.text("Speed trap", p.x + dir.x * 30 + 20, p.y + dir.y * 30 + 20);
+
         applet.noStroke();
         applet.strokeWeight(1);
     }
