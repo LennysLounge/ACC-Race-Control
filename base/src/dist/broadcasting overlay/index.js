21f10dfbe8a303419c004fade9f434746ddfedad
Leonard Schüngel <schuengel.leonard@googlemail.com>
2021-11-03 00:52:26 +0100

    Add a timing tower

--- base/src/dist/broadcasting overlay/index.js
+++ base/src/dist/broadcasting overlay/index.js
@@ -1,10 +1,36 @@
+// Socket that connects to ACC Race Control.
 ws = new WebSocket("ws://localhost:8080/socket");
+// entries in the timing tower, maps carId to element.
+var timingTowerEntries = {};
+var updatedIds = [];
+var focusedCarEntry = null;
+
 ws.onmessage = function (event) {
     let obj = JSON.parse(event.data);
 
     if (obj.type === "ClockMessage") {
         onClockMessage(obj);
+        onSessionUpdate();
+    } else if (obj.type === "CarStateMessage") {
+        onCarStateMessage(obj);
     }
+}
+
+function onSessionUpdate() {
+    for (const [key, value] of Object.entries(timingTowerEntries)) {
+        let found = false;
+        for (const id of updatedIds) {
+            if (id === key) {
+                found = true;
+            }
+        }
+        if (found === false) {
+            console.log("delete key", key, key in updatedIds);
+            value.parentNode.removeChild(value);
+            delete timingTowerEntries[key];
+        }
+    }
+    updatedIds = [];
 }
 
 function onClockMessage(msg) {
@@ -22,6 +48,73 @@ function onClockMessage(msg) {
     document.getElementsByClassName("clockLocalTime")[0].innerHTML
             = toLocalTime(localTime);
     document.getElementsByClassName("clockLocalTimeAMPM")[0].innerHTML = ampm;
+}
+
+function onCarStateMessage(msg) {
+    if (!(msg.id in timingTowerEntries)) {
+        let entry = createTimingTowerEntry();
+        let nameElement = entry.querySelector(".timingTowerEntryName");
+        let numberElement = entry.querySelector(".timingTowerEntryNumber");
+        nameElement.innerHTML = msg.name;
+        numberElement.innerHTML = msg.number;
+
+        let numberBox = entry.querySelector(".timingTowerEntryNumberBox");
+        if (msg.driverCategory == "PLATINUM" || msg.driverCategory == "GOLD") {
+            numberBox.classList.add("timingTowerEntryNumberGold");
+        } else if (msg.driverCategory == "SILVER") {
+            numberBox.classList.add("timingTowerEntryNumberSilver");
+        } else if (msg.driverCategory == "BRONZE") {
+            numberBox.classList.add("timingTowerEntryNumberBronze");
+        }
+        console.log(msg.driverCategory);
+
+        timingTowerEntries[msg.id] = entry;
+        document.getElementsByClassName("timingTowerList")[0].append(entry);
+    }
+    let entry = timingTowerEntries[msg.id];
+    let positionElement = entry.querySelector(".timingTowerEntryPosition");
+    entry.style.order = msg.pos;
+    positionElement.innerHTML = msg.pos;
+
+    if (msg.isFocused === "true") {
+        if (focusedCarEntry !== null) {
+            focusedCarEntry.querySelector(".timingTowerEntryPositionBox")
+                    .classList.remove("timingTowerEntryPositionBoxFocused");
+        }
+        entry.querySelector(".timingTowerEntryPositionBox")
+                .classList.add("timingTowerEntryPositionBoxFocused");
+        focusedCarEntry = entry;
+    }
+
+    updatedIds.push(msg.id);
+}
+
+function createTimingTowerEntry() {
+    let base = document.createElement("div");
+    base.classList.add("timingTowerEntry");
+
+    let positionBox = document.createElement("div");
+    positionBox.classList.add("timingTowerEntryPositionBox");
+    let position = document.createElement("div");
+    position.classList.add("timingTowerEntryPosition");
+    positionBox.append(position);
+
+    let nameBox = document.createElement("div");
+    nameBox.classList.add("timingTowerEntryNameBox");
+    let name = document.createElement("div");
+    name.classList.add("timingTowerEntryName");
+    nameBox.append(name);
+
+    let numberBox = document.createElement("div");
+    numberBox.classList.add("timingTowerEntryNumberBox");
+    let number = document.createElement("div");
+    number.classList.add("timingTowerEntryNumber");
+    numberBox.append(number)
+
+    base.append(positionBox);
+    base.append(nameBox);
+    base.append(numberBox);
+    return base;
 }
 
 function toDuration(millis) {
--- base/src/main/java/racecontrol/client/extension/broadcastingoverlay/BroadcastingOverlayExtension.java
+++ base/src/main/java/racecontrol/client/extension/broadcastingoverlay/BroadcastingOverlayExtension.java
@@ -10,11 +10,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.logging.Level;
 import racecontrol.client.extension.broadcastingoverlay.http.HTTPServer;
 import java.util.logging.Logger;
+import racecontrol.client.AccBroadcastingClient;
 import racecontrol.client.AccBroadcastingExtension;
+import racecontrol.client.data.CarInfo;
 import racecontrol.client.data.SessionInfo;
 import racecontrol.client.events.RealtimeUpdateEvent;
+import racecontrol.client.extension.broadcastingoverlay.messages.CarStateMessage;
 import racecontrol.client.extension.broadcastingoverlay.messages.ClockMessage;
 import racecontrol.client.extension.broadcastingoverlay.websocket.WebSocketServer;
+import racecontrol.client.extension.statistics.CarProperties;
+import static racecontrol.client.extension.statistics.CarProperties.CAR_ID;
+import static racecontrol.client.extension.statistics.CarProperties.CAR_NUMBER;
+import static racecontrol.client.extension.statistics.CarProperties.IS_FOCUSED_ON;
+import static racecontrol.client.extension.statistics.CarProperties.NAME;
+import static racecontrol.client.extension.statistics.CarProperties.REALTIME_POSITION;
+import racecontrol.client.extension.statistics.StatisticsExtension;
 import racecontrol.eventbus.Event;
 import racecontrol.eventbus.EventBus;
 import racecontrol.eventbus.EventListener;
@@ -34,6 +44,14 @@ public class BroadcastingOverlayExtension
      * This class's logger.
      */
     private static final Logger LOG = Logger.getLogger(BroadcastingOverlayExtension.class.getName());
+    /**
+     * Reference to the client.
+     */
+    private final AccBroadcastingClient client;
+    /**
+     * Statistics extension.
+     */
+    private final StatisticsExtension statistics;
     /**
      * Port to listen to.
      */
@@ -69,6 +87,8 @@ public class BroadcastingOverlayExtension
 
     private BroadcastingOverlayExtension() {
         EventBus.register(this);
+        client = AccBroadcastingClient.getClient();
+        statistics = StatisticsExtension.getInstance();
         objectMapper = new ObjectMapper();
     }
 
@@ -91,6 +111,20 @@ public class BroadcastingOverlayExtension
 
             String clockMessageJson = objectMapper.writeValueAsString(clockMessage);
             webSocketServer.sendMessage(clockMessageJson);
+
+            for (CarInfo car : client.getModel().getCarsInfo().values()) {
+                var stats = statistics.getCar(car.getCarId());
+                var stateMessage = new CarStateMessage();
+                stateMessage.setProperty("id", String.valueOf(stats.get(CAR_ID)));
+                stateMessage.setProperty("number", String.valueOf(stats.get(CAR_NUMBER)));
+                stateMessage.setProperty("pos", String.valueOf(stats.get(REALTIME_POSITION)));
+                stateMessage.setProperty("isFocused", String.valueOf(stats.get(IS_FOCUSED_ON)));
+                stateMessage.setProperty("name", stats.get(NAME));
+                stateMessage.setProperty("driverCategory", stats.get(CarProperties.CATEGORY).name());
+
+                String stateMessageJson = objectMapper.writeValueAsString(stateMessage);
+                webSocketServer.sendMessage(stateMessageJson);
+            }
 
         } catch (JsonProcessingException e) {
             LOG.log(Level.WARNING, "Error processing json", e);
