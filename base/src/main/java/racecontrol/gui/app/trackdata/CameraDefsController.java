94b890f5c2bb7d1f1862f846c00d472041de4334
Leonard Schüngel <schuengel.leonard@googlemail.com>
2022-04-14 01:35:46 +0200

    Add a camera tab to the track data

--- base/src/main/java/racecontrol/gui/app/trackdata/CameraDefsController.java
+++ base/src/main/java/racecontrol/gui/app/trackdata/CameraDefsController.java
@@ -5,16 +5,104 @@
  */
 package racecontrol.gui.app.trackdata;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static racecontrol.client.AccBroadcastingClient.getClient;
+import racecontrol.client.events.RealtimeUpdateEvent;
+import racecontrol.client.events.TrackInfoEvent;
+import racecontrol.client.model.Model;
+import racecontrol.client.protocol.SessionInfo;
+import racecontrol.eventbus.Event;
+import racecontrol.eventbus.EventBus;
+import racecontrol.eventbus.EventListener;
+
 /**
  *
  * @author Leonard
  */
-public class CameraDefsController {
+public class CameraDefsController
+        implements EventListener {
 
     private final CameraDefsPanel panel = new CameraDefsPanel();
 
+    private Map<String, Map<String, List<Float>>> camChanges = new HashMap<>();
+
+    private String currentCamSet = "";
+    private String currentCam = "";
+    private int currentFocusedCar = 0;
+
+    public CameraDefsController() {
+        EventBus.register(this);
+
+        camChanges.put("set1", new HashMap<>());
+        camChanges.put("set2", new HashMap<>());
+        panel.camChanges = camChanges;
+    }
+
     public CameraDefsPanel getPanel() {
         return panel;
+    }
+
+    @Override
+    public void onEvent(Event e) {
+        if (e instanceof RealtimeUpdateEvent) {
+            SessionInfo info = ((RealtimeUpdateEvent) e).getSessionInfo();
+
+            float currentSplinePos = 0;
+            Model model = getClient().getModel();
+            if (model.cars.containsKey(info.getFocusedCarIndex())) {
+                currentSplinePos = model.cars.get(info.getFocusedCarIndex()).splinePosition;
+            }
+
+            panel.currentSplinePos = currentSplinePos;
+
+            panel.currentCamSet.setText(info.getActiveCameraSet());
+            panel.currentCam.setText(info.getActiveCamera());
+            panel.invalidate();
+
+            logCamChanges(info, currentSplinePos);
+
+            currentCamSet = info.getActiveCameraSet();
+            currentCam = info.getActiveCamera();
+            currentFocusedCar = info.getFocusedCarIndex();
+        } else if (e instanceof TrackInfoEvent) {
+            Map<String, List<String>> camSets = ((TrackInfoEvent) e).getInfo().getCameraSets();
+            panel.camSets = camSets;
+            camSets.get("set1").forEach(cam -> camChanges.get("set1").put(cam, new ArrayList<>()));
+            camSets.get("set2").forEach(cam -> camChanges.get("set2").put(cam, new ArrayList<>()));
+        }
+    }
+
+    private void logCamChanges(SessionInfo info, float currentSplinePos) {
+
+        // if the focus changed we dont want to log the cam change.
+        if (currentFocusedCar != info.getFocusedCarIndex()) {
+            return;
+        }
+
+        // if the cam set changed we dont want to log.
+        String newCamSet = info.getActiveCameraSet();
+        if (!newCamSet.equals(currentCamSet)) {
+            return;
+        }
+
+        // if the cam set is not "set1" or "set2" then dont log
+        if (!newCamSet.equals("set1") && !newCamSet.equals("set2")) {
+            return;
+        }
+
+        // if the new cam did not change we dont log.
+        String newCam = info.getActiveCamera();
+        if (newCam.equals(currentCam)) {
+            return;
+        }
+        
+        // camera did change. Record current car position
+        if (camChanges.get(newCamSet).containsKey(newCam)) {
+            camChanges.get(newCamSet).get(newCam).add(currentSplinePos);
+        }
     }
 
 }
--- base/src/main/java/racecontrol/gui/app/trackdata/CameraDefsPanel.java
+++ base/src/main/java/racecontrol/gui/app/trackdata/CameraDefsPanel.java
@@ -5,8 +5,19 @@
  */
 package racecontrol.gui.app.trackdata;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import processing.core.PApplet;
+import static processing.core.PConstants.CENTER;
+import static processing.core.PConstants.LEFT;
+import static racecontrol.gui.LookAndFeel.COLOR_BLACK;
+import static racecontrol.gui.LookAndFeel.COLOR_DARK_GRAY;
+import static racecontrol.gui.LookAndFeel.COLOR_WHITE;
+import static racecontrol.gui.LookAndFeel.LINE_HEIGHT;
+import static racecontrol.gui.LookAndFeel.TEXT_SIZE;
 import racecontrol.gui.lpui.LPContainer;
+import racecontrol.gui.lpui.LPLabel;
 
 /**
  *
@@ -15,13 +26,66 @@ import racecontrol.gui.lpui.LPContainer;
 public class CameraDefsPanel
         extends LPContainer {
 
+    protected final LPLabel currentCamSet = new LPLabel("Camset");
+    protected final LPLabel currentCam = new LPLabel("Cam");
+
+    protected Map<String, Map<String, List<Float>>> camChanges = new HashMap<>();
+    protected Map<String, List<String>> camSets = new HashMap<>();
+
+    protected float currentSplinePos = 0;
+
     public CameraDefsPanel() {
         setName("Camera Defs");
+
+        currentCamSet.setPosition(20, 0);
+        addComponent(currentCamSet);
+        currentCam.setPosition(120, 0);
+        addComponent(currentCam);
     }
 
     @Override
     public void draw(PApplet applet) {
+        applet.fill(COLOR_DARK_GRAY);
+        applet.rect(0, 0, getWidth(), getHeight());
 
+        applet.translate(20, LINE_HEIGHT + 20);
+        drawCamGraph(applet, "set1");
+        applet.translate(0, 220);
+        drawCamGraph(applet, "set2");
+        applet.translate(-20, -LINE_HEIGHT - 20 - 220);
+
+    }
+
+    private void drawCamGraph(PApplet applet, String camSet) {
+        float w = getWidth() - 40;
+        float h = 200;
+        applet.fill(COLOR_BLACK);
+        applet.rect(0, 0, w, h);
+        if (camSets.containsKey(camSet)) {
+            applet.fill(COLOR_WHITE);
+            applet.textAlign(LEFT, CENTER);
+            applet.textSize(TEXT_SIZE * 0.6f);
+
+            for (int i = 0; i < camSets.get(camSet).size(); i++) {
+                applet.fill(COLOR_WHITE);
+                applet.noStroke();
+                String cam = camSets.get(camSet).get(i);
+                float x = (w - 40) / camSets.get(camSet).size() * i;
+                applet.text(cam, x + 40, LINE_HEIGHT * (i % 2 == 0 ? 0.75f : 0.25f));
+
+                applet.stroke(COLOR_WHITE);
+                applet.noFill();
+                int c = 0;
+                for (Float changePos : camChanges.get(camSet).get(cam)) {
+                    x = PApplet.map(changePos, 0, 1, 40, w);
+                    applet.ellipse(x, LINE_HEIGHT + c * 5, 5, 5);
+                    c++;
+                }
+            }
+            float x = PApplet.map(currentSplinePos, 0, 1, 40, w);
+            applet.stroke(COLOR_WHITE);
+            applet.line(x, 0, x, h);
+        }
     }
 
 }
