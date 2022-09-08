15eb701a3884682131a5d87c8e76b283b1772bfc
Leonard Schüngel <schuengel.leonard@gmail.com>
2022-09-08 23:10:48 +0200

    Add client extensions to the event bus by default

--- base/src/main/java/racecontrol/client/ClientExtension.java
+++ base/src/main/java/racecontrol/client/ClientExtension.java
@@ -6,22 +6,43 @@
 package racecontrol.client;
 
 import racecontrol.client.model.Model;
+import racecontrol.eventbus.EventBus;
+import racecontrol.eventbus.EventListener;
 
 /**
  * Base class for an extension to the client.
+ *
  * @author Leonard
  */
-public abstract class ClientExtension {
+public abstract class ClientExtension
+        implements EventListener {
 
     /**
      * Reference to the writable data model.
      */
     private Model writabelModel = new Model();
 
+    /**
+     * A Client extension is registered to the event bus by default.
+     */
+    public ClientExtension() {
+        EventBus.register(this);
+    }
+
+    /**
+     * Returns the writable version of the system model.
+     *
+     * @return The writable version of the system model.
+     */
     protected Model getWritableModel() {
         return writabelModel;
     }
 
+    /**
+     * Sets the writable version of the system model.
+     *
+     * @param model The writable version of the system model.
+     */
     protected void setWritableModel(Model model) {
         this.writabelModel = model;
     }
